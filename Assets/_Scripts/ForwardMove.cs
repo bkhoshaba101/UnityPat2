9b030786687160ad3d0c6c83a53aacf414ead1f1
bkhoshaba101 <bkhoshaba101@pointloma.edu>
2016-11-02 14:28:51 -0700

    first commit

---
+++ Assets/_Scripts/ForwardMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardMove : MonoBehaviour {
+
+	public TrainState instance;
+
+	void OnMouseDown() 
+	{/*
+		if (instance.hold == 2 || instance.hold == 4)
+			instance.ph = 0;
+		else if (instance.hold == 1 || instance.hold == 3)
+			instance.ph = 2;
+		else*/
+			instance.ph = 1;	
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# UnityPat2
